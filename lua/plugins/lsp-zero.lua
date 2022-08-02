780a89f6b0677d59ba5ba73f84841c97cd99d06f
Quan Cao <quancao2802@gmail.com>
2022-08-02 12:01:04 +0700

    Add lsp window scroll shortcut

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -6,6 +6,6 @@ local cmp = require "cmp"
 cmp.setup {
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4)
   })
 }
