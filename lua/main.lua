c1a558a145ab0450fb6c8a2367cbc4ad2c1eebe9
Quan Cao <quancao2802@gmail.com>
2022-12-24 14:47:21 +0700

    feat: add lsp error message inline

--- lua/main.lua
+++ lua/main.lua
@@ -46,23 +46,23 @@ return packer.startup(function(use)
   -- Tree - File explorer
   use {
     "kyazdani42/nvim-tree.lua",
-    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    requires = { "kyazdani42/nvim-web-devicons" },
     tag = "nightly",
     config = [[ require "plugins.nvim-tree" ]]
   }
 
   -- Gitsigns - Show git diff
   use {
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      require("gitsigns").setup()
     end
   }
 
   -- Lualine
   use {
     "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    requires = { "kyazdani42/nvim-web-devicons" },
     config = [[ require "plugins.lualine" ]]
   }
 
@@ -76,23 +76,23 @@ return packer.startup(function(use)
 
   -- LSP
   use {
-    'VonHeikemen/lsp-zero.nvim',
+    "VonHeikemen/lsp-zero.nvim",
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/nvim-lsp-installer'},
+      {"neovim/nvim-lspconfig"},
+      {"williamboman/nvim-lsp-installer"},
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      {"hrsh7th/nvim-cmp"},
+      {"hrsh7th/cmp-buffer"},
+      {"hrsh7th/cmp-path"},
+      {"saadparwaiz1/cmp_luasnip"},
+      {"hrsh7th/cmp-nvim-lsp"},
+      {"hrsh7th/cmp-nvim-lua"},
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      {"L3MON4D3/LuaSnip"},
+      {"rafamadriz/friendly-snippets"},
     },
     config = [[ require "plugins.lsp-zero" ]]
   }
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -2,6 +2,17 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 lsp.setup()
 
+vim.opt.signcolumn = "yes" -- Reserve space for diagnostic icons
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
 local cmp = require "cmp"
 cmp.setup {
   mapping = cmp.mapping.preset.insert({
