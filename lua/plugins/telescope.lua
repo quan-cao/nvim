8d61621401ef91c1d5610752d1456f346035312b
Quan Cao <quancao2802@gmail.com>
2023-02-08 22:39:01 +0700

    add shortcut for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,4 +4,5 @@ local map = vim.api.nvim_set_keymap
 require("telescope").setup()
 
 map("n", "<Leader>ff", [[ :Telescope find_files<CR> ]], {})
+map("n", "<Leader>fg", [[ :Telescope live_grep<CR> ]], {})
 map("n", "<Leader>fb", [[ :Telescope buffers<CR> ]], {})
