c6a3f506cc94d9a5c34e6513ef3a2ed2923073ff
Quan Cao <quancao2802@gmail.com>
2023-04-30 22:33:45 +0700

    change map for jump to definition

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ end
 
 local on_attach = function(client, bufnr)
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 end
 
 -- Pyright
