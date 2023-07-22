350054e19b5c0b135da3d9f0fa143da56d2e7c32
Quan Cao <quancao2802@gmail.com>
2023-07-22 12:57:53 +0700

    add vsnip plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,9 @@
 -----------------------------------------------------------
 
 -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-vim.cmd("colorscheme catppuccin-macchiato")
-
 -- dayfox, dawnfox, duskfox, terafox, nightfox
--- vim.cmd("colorscheme nightfox")
-
-
 -- onedark, onelight, onedark_vivid, onedark_dark
--- vim.cmd("colorscheme onedark")
+vim.cmd("colorscheme catppuccin-macchiato")
 
 -----------------------------------------------------------
 -- Setup
